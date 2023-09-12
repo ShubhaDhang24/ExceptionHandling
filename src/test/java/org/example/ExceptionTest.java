45c584dedbefa9897a2eb1dcb29b1cbb9916fbbe
Shubha Dhang <shubhadhang@gmail.com>
2023-09-12 13:24:05 +0200

    Checked and unchecked exception

---
+++ src/test/java/org/example/ExceptionTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExceptionTest {
+
+    @Test
+    void ex1(){
+    }
+}
